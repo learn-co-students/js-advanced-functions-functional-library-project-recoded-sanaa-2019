2dabf067ecc8830734a2364f08eb2594f5fb4f37
Atheeralaghbari <atheeralaghbari61@gmail.com>
2019-12-09 16:36:55 -0800

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,34 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback){
+      for(index in collection){
+        callback( collection[index], index, collection)
+      } 
     },
 
     map: function() {
-
+      let myArr=[]
+      for(index in collection){
+        let element = collection[index]
+        myArr.push(callback(element, index, collection))}
     },
 
     reduce: function() {
-
+   
+      if(acc != null){
+         return  collection.reduce(callback,acc);
+      }else{
+         return collection.reduce(callback);
+      }
+    
+    },
+    size: function(collection){
+      let myarr = [];
+      for(let i=0;i< myArr.length;i++){
+        myarr.push(collection[i])
+      }
+      return myarr.length;
     },
 
     functions: function() {
